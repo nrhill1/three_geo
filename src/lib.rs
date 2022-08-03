8bf0b5f50dc59c7f00b922815b9a6faafde62e78
Nic Hill <nrhill1@gmail.com>
2022-08-03 08:59:35 -0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn format_coords(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn 03_geo(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(format_coords, m)?)?;
+    Ok(())
+}
