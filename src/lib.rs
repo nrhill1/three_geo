69dda970723e432accbb74986324018d6f87e763
Nic Hill <nrhill1@gmail.com>
2022-08-03 18:49:20 -0700

    creating function for 3d coordinate (Point)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,35 @@
 
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn format_coords(a: usize, b: usize) -> PyResult<String> {
-    Ok(format!("{} {}", a, b))
-}
 
-/// A Python module implemented in Rust.
+
 #[pymodule]
-fn three_geo(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(format_coords, m)?)?;
+fn three_geo(py: Python, m: &PyModule) -> PyResult<()> {
+
+    // Point definition
+    #[pyclass]
+    struct Point {
+        #[pyo3(get)]
+        x: f64,
+        #[pyo3(get)]
+        y: f64,
+        #[pyo3(get)]
+        z: f64
+    }
+
+    // Point implementation
+    #[pymethods]
+    impl Point {
+        #[new]
+        pub fn new(x: f64, y: f64, z: f64) -> Self {
+            Point { x, y, z}
+        }
+    }
+
+    // #[pyfn(m, "format_coords")]
+    // fn format_coords(_py: Python, x:f64, y:f64, z:f64) -> PyResult<String> {
+        
+    // }
+
     Ok(())
 }
