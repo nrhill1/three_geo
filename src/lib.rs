9e3cda5130b0d69d0941aedbcdc11a20fc62ccb3
Nic Hill <nrhill1@gmail.com>
2022-08-04 10:18:53 -0700

    removing pyramid param declarations

--- src/lib.rs
+++ src/lib.rs
@@ -77,12 +77,7 @@ impl Pyramid {
 
         let base: Vec<Point> = create_base(base_length, x_off, y_off);
 
-        return Pyramid{base_length, 
-                       height, 
-                       apex, 
-                       base, 
-                       x_off, 
-                       y_off}
+        return Pyramid{base_length, height, apex, base, x_off, y_off}
     }
 
     pub fn surface_area(&self) -> f64 {
