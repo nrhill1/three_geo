791e2625cb5f0d8e7d2d9ec7584fcf714a05a560
Nic Hill <nrhill1@gmail.com>
2022-08-03 20:25:46 -0700

    finishing up new Pyramid struct

--- src/lib.rs
+++ src/lib.rs
@@ -25,27 +25,45 @@ impl Point {
 struct Pyramid {
     // Parameters for new pyramid
     #[pyo3(get)]
-    apex: Point,
-    #[pyo3(get)]
     base_length: f64,
     #[pyo3(get)]
-    hypotenuse: f64,
-    // Points for the base of the pyramid
+    height: f64,
+    // Apex & base of the pyramid
     #[pyo3(get)]
-    p1: Point,
+    apex: Point,
     #[pyo3(get)]
-    p2: Point,
-    #[pyo3(get)]
-    p3: Point,
-    #[pyo3(get)]
-    p4: Point
+    base: arr[Point;4] 
 }
 
 #[pymethods]
 impl Pyramid {
     #[new]
-    pub fn new(apex: Point, base_length: f64, hypotenuse: f64) -> Self {
-        Pyramid {apex:, }
+    pub fn new(base_length: f64, height: f64) -> Self {
+        
+        fn create_apex(&hgt: &f64) -> Point {
+            Point{0, 0, &hgt}
+        }
+
+        let apex = create_apex(&height);
+
+        fn create_base(&bl: &f64) {
+            
+            let sqrt = f64::sqrt;
+            let bh = sqrt(&bl);
+
+            let p1: Point = Point{x: &bh * -1, y: &bh * -1, z: 0};
+            let p2: Point = Point{x: p1.x, y: p1.y - &bl, z: 0};
+            let p3: Point = Point{x: p2.x + &bl, y: p2.y, z: 0};
+            let p4: Point = Point{x: p3.x, y: p3.y + &bl, z: 0};
+
+            let base: arr[Point;4] = [p1, p2, p3, p4]
+
+            return base;
+        }
+
+        let base = create_base(&base_length)
+
+        return Pyramid{base_length, height, apex, base}
     }
 }
 
@@ -53,7 +71,7 @@ impl Pyramid {
 #[pymodule]
 fn three_geo(_: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Point>()?;
-
+    m.add_class::<Point>()?;
 
     Ok(())
 }
