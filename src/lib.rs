f3e647a3932d4958cbe6471b16850b327916fec5
Nic Hill <nrhill1@gmail.com>
2022-08-03 19:29:47 -0700

    implementing Pyramid

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,56 @@
 
 use pyo3::prelude::*;
 
+// Point definition
+#[pyclass]
+struct Point {
+    #[pyo3(get)]
+    x: f64,
+    #[pyo3(get)]
+    y: f64,
+    #[pyo3(get)]
+    z: f64
+}
+
+// Point implementation
+#[pymethods]
+impl Point {
+    #[new]
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Point { x, y, z}
+    }
+}
+
+#[pyclass]
+struct Pyramid {
+    // Parameters for new pyramid
+    #[pyo3(get)]
+    apex: Point,
+    #[pyo3(get)]
+    base_length: f64,
+    #[pyo3(get)]
+    hypotenuse: f64,
+    //
+    #[pyo3(get)]
+    p1: Point,
+    #[pyo3(get)]
+    p2: Point,
+    #[pyo3(get)]
+    p3: Point,
+    #[pyo3(get)]
+    p4: Point
+}
+
+#[pymethods]
+impl Pyramid {
+    #[new]
+    pub fn new()
+}
 
 
 #[pymodule]
-fn three_geo(py: Python, m: &PyModule) -> PyResult<()> {
-
-    // Point definition
-    #[pyclass]
-    struct Point {
-        #[pyo3(get)]
-        x: f64,
-        #[pyo3(get)]
-        y: f64,
-        #[pyo3(get)]
-        z: f64
-    }
-
-    // Point implementation
-    #[pymethods]
-    impl Point {
-        #[new]
-        pub fn new(x: f64, y: f64, z: f64) -> Self {
-            Point { x, y, z}
-        }
-    }
-    
-
-    // #[pyfn(m, "format_coords")]
-    // fn format_coords(_py: Python, x:f64, y:f64, z:f64) -> PyResult<String> {
-        
-    // }
+fn three_geo(_: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Point>()?;
 
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-from .three_geo import Point
+from three_geo import Point
 
 new_point = Point(1, 2, 3.4)
 
-print(new_point.x, new_point.y, new_point.z)
+print("X: " + str(new_point.x), 
+      "Y: " + str(new_point.y), 
+      "Z: " + str(new_point.z))
 
